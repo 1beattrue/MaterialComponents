f9b79354bfdc87c41770e3352b5ca7f45558cbb7
Vitaly <onebeattrue@gmail.com>
2023-10-15 02:44:39 +0300

    Scaffold

--- app/src/main/java/edu/mirea/onebeattrue/materialcomponents/MainActivity.kt
+++ app/src/main/java/edu/mirea/onebeattrue/materialcomponents/MainActivity.kt
@@ -3,18 +3,30 @@ package edu.mirea.onebeattrue.materialcomponents
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.AlertDialog
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Favorite
+import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,48 +39,65 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-private fun Test() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Example3()
-    }
-}
-
-@Composable
-private fun Example1() {
-    OutlinedButton(onClick = { /*TODO*/ }) {
-        Text(text = "Outlined Button")
-    }
-}
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun Example2() {
-    TextField(
-        value = "Value",
-        onValueChange = {},
-        label = { Text(text = "Label") }
-    )
-}
-
-@Composable
-private fun Example3() {
-    AlertDialog(
-        onDismissRequest = {},
-        title = { Text(text = "Are you sure?") },
-        text = { Text(text = "Do you want to delete this file?") },
-        confirmButton = {
-            TextButton(onClick = { /*TODO*/ }) {
-                Text(text = "Yes")
-            }
-        },
-        dismissButton = {
-            TextButton(onClick = { /*TODO*/ }) {
-                Text(text = "No")
-            }
+private fun Test() {
+    ModalNavigationDrawer(drawerContent = {
+        Column(
+            Modifier
+                .background(Color.White)
+                .fillMaxHeight()
+                .width(128.dp)
+        ) {
+            Text(text = "item 1")
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "item 2")
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "item 3")
         }
-    )
+    }) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text(text = "Top App Bar Title") },
+                    navigationIcon = {
+                        IconButton(onClick = {}) {
+                            Icon(Icons.Rounded.Menu, contentDescription = null)
+                        }
+                    }
+                )
+            },
+            bottomBar = {
+                NavigationBar {
+                    val items = listOf("item 1", "item 2", "item 3")
+
+                    NavigationBarItem(
+                        icon = { Icon(Icons.Rounded.Favorite, contentDescription = null) },
+                        label = { Text(items[0]) },
+                        selected = true,
+                        onClick = {}
+                    )
+
+                    NavigationBarItem(
+                        icon = { Icon(Icons.Rounded.Favorite, contentDescription = null) },
+                        label = { Text(items[0]) },
+                        selected = false,
+                        onClick = {}
+                    )
+
+                    NavigationBarItem(
+                        icon = { Icon(Icons.Rounded.Favorite, contentDescription = null) },
+                        label = { Text(items[0]) },
+                        selected = false,
+                        onClick = {}
+                    )
+                }
+            }
+        ) {
+            Text(
+                text = "This is Scaffold content",
+                modifier = Modifier.padding(it)
+            )
+        }
+    }
 }
