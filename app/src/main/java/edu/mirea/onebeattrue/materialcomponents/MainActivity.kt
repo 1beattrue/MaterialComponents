d52524efae1fc8f0fed5f437f3e986f401a1f125
Vitaly <onebeattrue@gmail.com>
2023-10-15 01:42:01 +0300

    Создание собственной extension функции

--- app/src/main/java/edu/mirea/onebeattrue/materialcomponents/MainActivity.kt
+++ app/src/main/java/edu/mirea/onebeattrue/materialcomponents/MainActivity.kt
@@ -4,30 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import edu.mirea.onebeattrue.materialcomponents.ui.theme.MaterialComponentsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(
-                // modifier = Modifier.weight(1f), метод не доступен, поскольку он расширяется в ColumnScope Modifier.weight()
-                text = "Text"
-            )
             Column {
-                Text(
-                    modifier = Modifier.weight(1f),
-                    text = "Text"
-                )
+                TestText(text = "text", count = 5)
             }
         }
     }
 }
 
+@Composable
+private fun ColumnScope.TestText(
+    text: String,
+    count: Int
+) {
+    repeat(count) {
+        Text(
+            modifier = Modifier.weight(1f),
+            text = text
+        )
+    }
+}
