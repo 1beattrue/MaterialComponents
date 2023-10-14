0539553d6fdd2ae17a6dcd34446e801c11abe57c
Vitaly <onebeattrue@gmail.com>
2023-10-15 02:06:40 +0300

    Несколько примеров функций Material Components

--- app/src/main/java/edu/mirea/onebeattrue/materialcomponents/MainActivity.kt
+++ app/src/main/java/edu/mirea/onebeattrue/materialcomponents/MainActivity.kt
@@ -3,10 +3,17 @@ package edu.mirea.onebeattrue.materialcomponents
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
@@ -14,21 +21,54 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Column {
-                TestText(text = "text", count = 5)
+                Test()
             }
         }
     }
 }
 
 @Composable
-private fun ColumnScope.TestText(
-    text: String,
-    count: Int
-) {
-    repeat(count) {
-        Text(
-            modifier = Modifier.weight(1f),
-            text = text
-        )
+private fun Test() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Example3()
     }
+}
+
+@Composable
+private fun Example1() {
+    OutlinedButton(onClick = { /*TODO*/ }) {
+        Text(text = "Outlined Button")
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun Example2() {
+    TextField(
+        value = "Value",
+        onValueChange = {},
+        label = { Text(text = "Label") }
+    )
+}
+
+@Composable
+private fun Example3() {
+    AlertDialog(
+        onDismissRequest = {},
+        title = { Text(text = "Are you sure?") },
+        text = { Text(text = "Do you want to delete this file?") },
+        confirmButton = {
+            TextButton(onClick = { /*TODO*/ }) {
+                Text(text = "Yes")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = { /*TODO*/ }) {
+                Text(text = "No")
+            }
+        }
+    )
 }
